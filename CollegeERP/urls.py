98b983a28599665eee59df3a8ccb5270eb661ecc
Sarvesh Patil <sarvesh2902@gmail.com>
2022-04-26 18:51:41 +0530

    Deploy commit

---
+++ CollegeERP/urls.py
@@ -0,0 +1,22 @@
+import imp
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.views.static import serve
+from django.conf.urls import url
+
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('info.urls')),
+    path('info/', include('info.urls')),
+    path('accounts/login/',
+         auth_views.LoginView.as_view(template_name='info/login.html'), name='login'),
+    path('accounts/logout/',
+         auth_views.LogoutView.as_view(template_name='info/logout.html'), name='logout'),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
+]
