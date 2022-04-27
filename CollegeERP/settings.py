3b0cb01c3e8a2f68d02ce13ca551458f501d2579
Sarvesh Patil <sarvesh2902@gmail.com>
2022-04-27 22:05:15 +0530

    heroku deploy

--- CollegeERP/settings.py
+++ CollegeERP/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = './static'
 
 LOGIN_REDIRECT_URL = '/'
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
